e355e4944f832c00ed4324fd973e94a9b3dea7b2
Eric Suescun <edsuescun@gmail.com>
2019-03-16 20:35:33 -0500

    Se implementa el Controller en PLURAL para ProductS

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,20 @@
+class ProductsController < ApplicationController
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(product_params)
+	 
+		if @product.save
+	 	 		redirect_to @product
+		else
+			render :new
+		end
+	end
+
+	private
+	  	def product_params
+	    	params.require(:product).permit(:name, :price, {category_ids: []})
+	  	end
+end
